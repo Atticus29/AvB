65ac034d2d95915bd583a46d12abd14de945f02e
Mark <mark.aaron.fisher@gmail.com>
2017-05-26 13:32:19 -0700

    fix layout of buttons in MainActivity

---
+++ app/src/main/java/com/epicodus/avb/ExperimentActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.avb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiment);
+    }
+}
