0bc8b8d332de1ae07b75fd91f2fc45f3469396a1
Mark <mark.aaron.fisher@gmail.com>
2017-05-26 15:22:58 -0700

    get buttons to navigate to the activities they need to get to

--- app/src/main/java/com/epicodus/avb/AllExperimentsActivity.java
+++ app/src/main/java/com/epicodus/avb/AllExperimentsActivity.java
@@ -1,13 +1,30 @@
 package com.epicodus.avb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AllExperimentsActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AllExperimentsActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.createButton) Button mCreateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_experiments);
+        ButterKnife.bind(this);
+        mCreateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mCreateButton){
+            Intent intent = new Intent(AllExperimentsActivity.this, AddExperimentActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/epicodus/avb/ExperimentActivity.java
+++ app/src/main/java/com/epicodus/avb/ExperimentActivity.java
@@ -1,9 +1,14 @@
 package com.epicodus.avb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ExperimentActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class ExperimentActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
