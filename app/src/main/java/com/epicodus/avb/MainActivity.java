8e3d4a6fecf9d15d9929052e1ec51c51b3104771
Mark <mark.aaron.fisher@gmail.com>
2017-05-26 11:17:20 -0700

    add background image, three buttons, and font to a TextView in MainActivity. Create aboutActivity and populate TextView with description from README

--- app/src/main/java/com/epicodus/avb/MainActivity.java
+++ app/src/main/java/com/epicodus/avb/MainActivity.java
@@ -1,13 +1,22 @@
 package com.epicodus.avb;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.appTitleText) TextView mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Typeface spaceAge = Typeface.createFromAsset(getAssets(), "fonts/spaceage.ttf");
+        mTitle.setTypeface(spaceAge);
     }
 }
