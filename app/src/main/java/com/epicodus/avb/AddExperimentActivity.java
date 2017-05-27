3d25f46e775314875fff4af0395272641431c1d2
Mark <mark.aaron.fisher@gmail.com>
2017-05-27 10:46:35 -0700

    finally get form validation to work

--- app/src/main/java/com/epicodus/avb/AddExperimentActivity.java
+++ app/src/main/java/com/epicodus/avb/AddExperimentActivity.java
@@ -3,12 +3,17 @@ package com.epicodus.avb;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
 
 public class AddExperimentActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.nameInput) EditText mNameInput;
@@ -16,6 +21,7 @@ public class AddExperimentActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.treatmentTwoName) EditText mTreatmentTwoName;
     @Bind(R.id.effectSizeInput) EditText mEffectSizeInput;
     @Bind(R.id.submitButton) Button mSubmitButton;
+    public static final String TAG =  AddExperimentActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +34,25 @@ public class AddExperimentActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View v){
         if(v == mSubmitButton){
-            Intent intent = new Intent(AddExperimentActivity.this, ExperimentActivity.class);
-            startActivity(intent);
+            String name = mNameInput.getText().toString();
+            Log.d("name", name);
+            String treatmentOne = mTreatmentOneName.getText().toString();
+            Log.d("tx1", treatmentOne);
+            String treatmentTwo = mTreatmentTwoName.getText().toString();
+            Log.d("tx2", treatmentTwo.getClass().toString());
+            String effectSizeInput = mEffectSizeInput.getText().toString();
+            Log.d("effectSize", effectSizeInput);
+            if(!name.matches("") && !treatmentOne.matches("") && !treatmentTwo.matches("") && !effectSizeInput.matches("")){
+                double effectSizeAsNumber = parseDouble(effectSizeInput);
+                Experiment newExperiment = new Experiment (name, treatmentOne, treatmentTwo, effectSizeAsNumber);
+                Log.d(TAG, String.valueOf(newExperiment));
+                Intent intent = new Intent(AddExperimentActivity.this, ExperimentActivity.class);
+                startActivity(intent);
+            } else{
+                Toast.makeText(AddExperimentActivity.this, "Please make sure to fill out all input fields!", Toast.LENGTH_SHORT).show();
+            }
+
+
         }
     }
 }
