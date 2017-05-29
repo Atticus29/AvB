f82a3e7bfe38ee308c363b21dc3064b55f1c5864
Mark <mark.aaron.fisher@gmail.com>
2017-05-28 19:20:10 -0700

    add custom font to individual experiment page and change text in about section

--- app/src/main/java/com/epicodus/avb/SingleTreatmentAdapter.java
+++ app/src/main/java/com/epicodus/avb/SingleTreatmentAdapter.java
@@ -2,6 +2,7 @@ package com.epicodus.avb;
 
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
