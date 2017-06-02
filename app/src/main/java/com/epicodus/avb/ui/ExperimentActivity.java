d4c3bebfea309981f4ec0aa24fb9bd82f43b13a3
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-06-02 13:53:07 -0700

    get second recyclerView to work and change color palette

--- app/src/main/java/com/epicodus/avb/ui/ExperimentActivity.java
+++ app/src/main/java/com/epicodus/avb/ui/ExperimentActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -26,6 +27,8 @@ public class ExperimentActivity extends AppCompatActivity implements View.OnClic
     @Bind(R.id.treatmentRecyclerView) RecyclerView treatmentRecyclerView;
     @Bind(R.id.tweetResultsButton) Button mTweetResultsButton;
 
+    private TreatmentRecylerViewListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +47,11 @@ public class ExperimentActivity extends AppCompatActivity implements View.OnClic
         mViewAllButton.setOnClickListener(this);
         mTweetResultsButton.setOnClickListener(this);
         ArrayList<Experiment> experiments = Experiment.allExperiments;
-        treatmentRecyclerView.setAdapter(new TreatmentRecylerViewListAdapter(this, treatments));
+        adapter = new TreatmentRecylerViewListAdapter(this, treatments);
+        treatmentRecyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ExperimentActivity.this);
+        treatmentRecyclerView.setLayoutManager(layoutManager);
+        treatmentRecyclerView.setHasFixedSize(true);
     }
 
     @Override
