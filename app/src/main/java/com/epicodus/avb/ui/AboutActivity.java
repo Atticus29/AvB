2232372326f1b9410052ade87c4d26acca110a1a
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-06-02 17:20:53 -0700

    make textView of how it works section scrollable

--- app/src/main/java/com/epicodus/avb/ui/AboutActivity.java
+++ app/src/main/java/com/epicodus/avb/ui/AboutActivity.java
@@ -2,10 +2,17 @@ package com.epicodus.avb.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import com.epicodus.avb.R;
 
+import org.w3c.dom.Text;
+
+import butterknife.Bind;
+
 public class AboutActivity extends AppCompatActivity {
+    @Bind(R.id.aboutTextView) TextView aboutTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
