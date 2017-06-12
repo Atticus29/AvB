2d25b48c07663b55813a69ef7ff96c980e44f357
Mark <mark.aaron.fisher@gmail.com>
2017-06-11 21:23:31 -0700

    add passing calculateChiSquared method

--- app/src/main/java/com/epicodus/avb/models/Experiment.java
+++ app/src/main/java/com/epicodus/avb/models/Experiment.java
@@ -10,7 +10,7 @@ public class Experiment {
     public String name;
     public String treatmentOneName;
     public String treatmentTwoName;
-    public int treatmentOneSucesses;
+    public int treatmentOneSuccesses;
     public int treatmentTwoSucesses;
     public int treatmentOneFailures;
     public int treatmentTwoFailures;
@@ -26,7 +26,7 @@ public class Experiment {
         this.treatmentTwoName = treatmentTwoName;
         this.desiredEffectSize = desiredEffectSize;
         this.treatmentOneFailures = 0;
-        this.treatmentOneSucesses = 0;
+        this.treatmentOneSuccesses = 0;
         this.treatmentTwoFailures = 0;
         this.treatmentTwoSucesses = 0;
         this.minimumTrialsRequired = 25; //this is temporary and will be fleshed out with real statistics when time permits
@@ -44,8 +44,8 @@ public class Experiment {
         return treatmentTwoName;
     }
 
-    public int getTreatmentOneSucesses() {
-        return treatmentOneSucesses;
+    public int gettreatmentOneSuccesses() {
+        return treatmentOneSuccesses;
     }
 
     public int getTreatmentTwoSucesses() {
@@ -74,23 +74,22 @@ public class Experiment {
         this.pushId = pushId;
     }
 
-    public double calculateChiSquared(int treatmentOneSucesses, int treatmentOneFailures, int treatmentTwoSucesses, int treatmentTwoFailures){
-        int total_samples = treatmentOneSucesses + treatmentOneFailures + treatmentTwoSucesses + treatmentTwoFailures;
-        int total_successes = treatmentOneSucesses + treatmentTwoSucesses;
-        int total_treatmentOne_trials = treatmentOneSucesses + treatmentOneFailures;
+    public double calculateChiSquared(int treatmentOneSuccesses, int treatmentOneFailures, int treatmentTwoSucesses, int treatmentTwoFailures){
+        int total_samples = treatmentOneSuccesses + treatmentOneFailures + treatmentTwoSucesses + treatmentTwoFailures;
+        int total_successes = treatmentOneSuccesses + treatmentTwoSucesses;
+        int total_treatmentOne_trials = treatmentOneSuccesses + treatmentOneFailures;
         int total_failures = treatmentOneFailures + treatmentTwoFailures;
         int total_treatmentTwo_trials = treatmentTwoSucesses + treatmentTwoFailures;
 
-        double expTreatmentOneSuccess = (total_successes*total_treatmentOne_trials)/total_samples;
-        double expTreatmentOneFailures = (total_successes*total_treatmentTwo_trials)/total_samples;
-        double expTreatmentTwoSucesses = (total_failures*total_treatmentOne_trials)/total_samples;
-        double expTreatmentTwoFailures = (total_failures * total_treatmentTwo_trials)/total_samples;
+        double expTreatmentOneSuccesses = ((double)total_successes*total_treatmentOne_trials)/total_samples;
+        double expTreatmentTwoSucesses = ((double)total_successes*total_treatmentTwo_trials)/total_samples;
+        double expTreatmentOneFailures = ((double)total_failures*total_treatmentOne_trials)/total_samples;
+        double expTreatmentTwoFailures = ((double)total_failures * total_treatmentTwo_trials)/total_samples;
 
-
-        double chiSqSubunitOne = Math.pow(treatmentOneSucesses-expTreatmentOneSuccess ,2)/expTreatmentOneSuccess;
+        double chiSqSubunitOne = Math.pow(treatmentOneSuccesses-expTreatmentOneSuccesses ,2)/expTreatmentOneSuccesses;
         double chiSqSubunitSubTwo = Math.pow(treatmentTwoSucesses-expTreatmentTwoSucesses ,2)/expTreatmentTwoSucesses;
-        double chiSqSubunitFour = Math.pow(treatmentOneFailures-expTreatmentOneFailures ,2)/expTreatmentOneFailures;
-        double chiSqSubunitThree = Math.pow(treatmentTwoFailures-expTreatmentTwoFailures ,2)/expTreatmentTwoFailures;
+        double chiSqSubunitThree = Math.pow(treatmentOneFailures-expTreatmentOneFailures ,2)/expTreatmentOneFailures;
+        double chiSqSubunitFour = Math.pow(treatmentTwoFailures-expTreatmentTwoFailures ,2)/expTreatmentTwoFailures;
 
         double chiSq = chiSqSubunitOne + chiSqSubunitSubTwo + chiSqSubunitThree + chiSqSubunitFour;
 
